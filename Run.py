89bc73eb637f788fcf4a246c8991cf4f2532bd74
Zeo-shark <sauravberaiit@gmail.com>
2021-02-20 12:24:24 +0530

    resize the frame to max width of 100 pixels

--- Run.py
+++ Run.py
@@ -92,8 +92,7 @@ def run():
 			break
 
 		# resize the frame to have a maximum width of 500 pixels (the
-		# less data we have, the faster we can process it), then convert
-		# the frame from BGR to RGB for dlib
+
 		frame = imutils.resize(frame, width = 500)
 		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
